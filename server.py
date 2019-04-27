0cb91a00354de13a47f69ef4957724dd5d09d10c
Dialjini <danil_kustov@mail.ru>
2019-04-27 19:16:12 +0300

    add a+b method

---
+++ server.py
@@ -0,0 +1,51 @@
+import requests
+import cherrypy
+from cherrypy.lib import static
+import os, os
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return open('index.html')
+
+@cherrypy.expose
+class Heyo(object):
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self, a, b, ch):
+        if ch == '+':
+            return a + b
+        return "Wrong Command"
+
+conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd()),
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+        },
+            # 'tools.response_headers.headers': [('Content-Type', 'application/json')],
+        # '/generator': {
+        #     'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+        #     'tools.response_headers.on': True,
+        #     'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        # },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './puplic'
+        }
+    }
+
+
+cherrypy.config.update({'server.socket_host': '127.0.0.1',
+                            'server.socket_port': 8080,
+                            'tools.sessions.on': True,
+                            'engine.autoreload.on': False,
+                            'log.access_file': './access.log',
+                            'log.error_file': './error.log',
+                            })
+
+cherrypy.tree.mount(HelloWorld(), '/')
+cherrypy.tree.mount(Heyo(), '/g', conf)
+
+cherrypy.engine.start()
+cherrypy.engine.block()
