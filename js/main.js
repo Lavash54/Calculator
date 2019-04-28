31bdb039279e8c693d207ba6cb87badb61277dc7
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-04-28 14:55:06 +0700

    Creating a calculator form

---
+++ js/main.js
@@ -0,0 +1,63 @@
+var divBlocks = '<button class="block" id="$sing">$content</button>';
+
+
+function createBlocks() {
+  for (var i = 1; i < 36; i++) {
+    $('.panel').append(divBlocks
+      .replace('$sing', i)
+      .replace('$content', unicode(i))
+    );
+    if ((i > 10 && i < 14) || (i > 17) && (i < 21) || (i > 24) && (i < 28) || (i > 31) && (i < 34)) {
+      $('#' + i).removeClass('block');
+      $('#' + i).addClass('numbers');
+    } else if (i == 34) {
+      $('#' + i).removeClass('block');
+      $('#' + i).addClass('equally');
+    }
+  }
+}
+
+
+
+
+
+function unicode(i) {
+  switch(i) {
+    case 1: return 'Rad';
+    case 2: return 'Deg';
+    case 3: return '&#120;&#33;';
+    case 4: return '&#40;';
+    case 5: return '&#41;';
+    case 6: return '&#37;';
+    case 7: return 'AC';
+    case 8: return 'lnv';
+    case 9: return 'sin';
+    case 10: return 'ln';
+    case 11: return '&#55;';
+    case 12: return '&#56;';
+    case 13: return '&#57;';
+    case 14: return '&#47;';
+    case 15: return '&#960;';
+    case 16: return 'cos';
+    case 17: return 'log';
+    case 18: return '&#52;';
+    case 19: return '&#53;';
+    case 20: return '&#54;';
+    case 21: return '&#42;';
+    case 22: return '&#101;';
+    case 23: return 'tg';
+    case 24: return '&#8730;';
+    case 25: return '&#49;';
+    case 26: return '&#50;';
+    case 27: return '&#51;';
+    case 28: return '&#43;';
+    case 29: return 'Ans';
+    case 30: return 'EXP';
+    case 31: return 'x<sup>y</sup>';
+    case 32: return '&#48;';
+    case 33: return '&#46;';
+    case 34: return '&#61;';
+    case 35: return '&#45;';
+    default: return -1;
+  }
+}
