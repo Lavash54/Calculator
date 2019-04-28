0b95d5c7f022377f80fb21db1d10c6619a2f5b2e
Dialjini <danil_kustov@mail.ru>
2019-04-28 22:32:01 +0300

    formatting

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ import cherrypy
 from cherrypy.lib import static
 import os, os
 
+
 @cherrypy.expose
 class HelloWorld(object):
     @cherrypy.tools.accept(media='text/plain')
     def GET(self):
         return open(file='index.html', encoding='utf8')
+
 
 @cherrypy.expose
 class Heyo(object):
@@ -16,33 +18,33 @@ class Heyo(object):
             return a + b
         return "Wrong Command"
 
-conf = {
-        '/': {
-            'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd()),
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-        },
-            # 'tools.response_headers.headers': [('Content-Type', 'application/json')],
-        # '/generator': {
-        #     'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-        #     'tools.response_headers.on': True,
-        #     'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        # },
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': './public'
-        }
-    }
 
+conf = {
+    '/': {
+        'tools.sessions.on': True,
+        'tools.staticdir.root': os.path.abspath(os.getcwd()),
+        'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+        'tools.response_headers.on': True,
+    },
+    # 'tools.response_headers.headers': [('Content-Type', 'application/json')],
+    # '/generator': {
+    #     'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+    #     'tools.response_headers.on': True,
+    #     'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+    # },
+    '/static': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': './public'
+    }
+}
 
 cherrypy.config.update({'server.socket_host': '127.0.0.1',
-                            'server.socket_port': 8080,
-                            'tools.sessions.on': True,
-                            'engine.autoreload.on': False,
-                            'log.access_file': './access.log',
-                            'log.error_file': './error.log',
-                            })
+                        'server.socket_port': 8080,
+                        'tools.sessions.on': True,
+                        'engine.autoreload.on': False,
+                        'log.access_file': './access.log',
+                        'log.error_file': './error.log',
+                        })
 
 cherrypy.tree.mount(HelloWorld(), '/', conf)
 cherrypy.tree.mount(Heyo(), '/g', conf)
