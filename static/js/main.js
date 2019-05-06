bc46cb7b386e07f2b57e8da11ba9a70e25b4b92f
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-05-06 17:49:42 +0700

    Keyboard input

--- static/js/main.js
+++ static/js/main.js
@@ -35,9 +35,17 @@ $keys.click(function() {
       $summary.html($summary.html().replace(lastChar, keyValue));
     }
   } else if (keyValue == 'factorial') {
-    if (output != '') {
+    if (lastChar == '!') {
+      alert('Данный калькулятор не может вычислять двойной и более факториал');
+    } else if (output != '') {
       $summary.html($summary.html() + '!');
     }
+  } else if (keyValue == '0') {
+    if (output != '0') {
+      $summary.html($summary.html() + keyValue);
+    }
+  } else if (keyValue == 'delete') {
+
   } else {
     $summary.html($summary.html() + keyValue);
   }
@@ -50,3 +58,116 @@ function factorial(n) {
 function calculateString(str) {
   return (new Function('return ' + str))();
 }
+
+$(window).keydown(function(e) {
+  console.log(e.which);
+  switch (e.which) {
+    case 48:
+      key = 0;
+      break;
+    case 49:
+      key = 1;
+      break;
+    case 50:
+      key = 2;
+      break;
+    case 51:
+      key = 3;
+      break;
+    case 52:
+      key = 4;
+      break;
+    case 53:
+      key = 5;
+      break;
+    case 54:
+      key = 6;
+      break;
+    case 55:
+      key = 7;
+      break;
+    case 56:
+      key = 8;
+      break;
+    case 57:
+      key = 9;
+      break;
+
+    case 191:
+      key = '/';
+      break;
+    case 190:
+      key = '.';
+      break;
+    case 88:
+      key = '*';
+      break;
+    case 189:
+      key = '-';
+      break;
+    case 187:
+      key = '+';
+      break;
+    case 67:
+      key = 'clear';
+      break;
+
+    case 96:
+      key = 0;
+      break;
+    case 97:
+      key = 1;
+      break;
+    case 98:
+      key = 2;
+      break;
+    case 99:
+      key = 3;
+      break;
+    case 100:
+      key = 4;
+      break;
+    case 101:
+      key = 5;
+      break;
+    case 102:
+      key = 6;
+      break;
+    case 103:
+      key = 7;
+      break;
+    case 104:
+      key = 8;
+      break;
+    case 105:
+      key = 9;
+      break;
+
+    case 111:
+      key = '/';
+      break;
+    case 109:
+      key = '-';
+      break;
+    case 106:
+      key = '*';
+      break;
+    case 107:
+      key = '+';
+      break;
+    case 13:
+      key = '=';
+      break;
+    case 110:
+      key = '.';
+      break;
+    case 27:
+      key = 'clear';
+      break;
+    default:
+      return false;
+  }
+  $('[data-val="' + key + '"]').addClass('active').click();
+}).keyup(function() {
+  $('.active').removeClass('active');
+});
